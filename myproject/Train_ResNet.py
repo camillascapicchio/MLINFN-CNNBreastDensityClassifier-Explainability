1eb5839a77fdd604fb87376c7ae842e0cfab99c9
camillascapicchio <camillascapicchio@tiscali.it>
2021-06-14 09:33:29 +0200

    update

--- myproject/Train_ResNet.py
+++ myproject/Train_ResNet.py
@@ -4,7 +4,7 @@ import keras
 from keras import callbacks
 from keras import initializers
 from keras.layers import AveragePooling2D, Input, Dense, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, BatchNormalization
-from keras.layers.advanced_activations import LeakyReLU as LR
+from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Model
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 from keras import optimizers
@@ -36,36 +36,36 @@ cardinality = 1  #Cardinality of a ResNeXt. For ResNet set `cardinality` = 1.
 def residual_network(x):
 
     """
-     Defines the architecture of the residual convolutional neural network (ResNet).
+    	Defines the architecture of the residual convolutional neural network (ResNet).
 
-     Parameters
-     ----------
-     x : input of the network, tf.Tensor
+	Parameters
+	----------
+	x : input of the network, tf.Tensor
 	The input image of shape=(img_height, img_width, img_channels).
 
-     Returns
-     -------
-     x : output of the network, tf.Tensor
-        A tensor (1,4). The type of each element of the tensor is a float32 and it is the probability of the correspondent class.
-    
+	Returns
+	-------
+	x : output of the network, tf.Tensor
+	A tensor (1,4). The type of each element of the tensor is a float32 and it is the probability of the correspondent class.
+
     """
 
     def add_common_layers(y):
 
         """
-	 Defines a couple of subsequent layers frequently used through the model.
-	 Batch normalization applies a transformation that maintains the mean output close to 0 and the output standard deviation close to 1. The input is a tensor, the output is a tensor of same shape as 		 input.
-	 LeakyReLU is the activation function. The default slope coefficient is set to 0.3.
+	    Defines a couple of subsequent layers frequently used through the model.
+	    Batch normalization applies a transformation that maintains the mean output close to 0 and the output standard deviation close to 1. The input is a tensor, the output is a tensor of same 		    shape as input.
+	    LeakyReLU is the activation function. The default slope coefficient is set to 0.3.
 
-	 Parameters
-         ----------
-         y : input, tf.Tensor
+	    Parameters
+	    ----------
+	    y : input, tf.Tensor
 
-	 Returns
-     	 -------
-     	 y : output of the LeakyReLU layer, tf.Tensor
-            Same shape as the input.
-	    	
+	    Returns
+	    -------
+	    y : output of the LeakyReLU layer, tf.Tensor
+	    Same shape as the input.
+
 	"""
         y = layers.BatchNormalization()(y)
         y = layers.LeakyReLU()(y)
@@ -74,8 +74,8 @@ def residual_network(x):
     def grouped_convolution(y, nb_channels, _strides):
 
 	"""
-	 When `cardinality` == 1 this is just a standard convolution.
-	 This function is called in "residual_block" function.
+	    When `cardinality` == 1 this is just a standard convolution.
+	    This function is called in "residual_block" function.
 	    	
 	"""
         if cardinality == 1:
@@ -98,12 +98,12 @@ def residual_network(x):
 
     def residual_block(y, nb_channels_in, nb_channels_out, _strides=(1, 1), _project_shortcut=False):
         """
-        Our network consists of a stack of residual blocks. These blocks have the same topology,
-        and are subject to two simple rules:
-        - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
-        - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
+	    Our network consists of a stack of residual blocks. These blocks have the same topology,
+	    and are subject to two simple rules:
+	    - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
+	    - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
 
-	The input of each of the residual block is shared by two branches: in the first, it passes through several convolutional, batch normalization, activation and max pooling layers while in the other 		branch it passes through a convolutional layer and a batch normalization only. The outputs of these two branches are then added together to constitute the residual block and then LeakyReLu is 	performed on the the output of the residual block.
+	    The input of each of the residual block is shared by two branches: in the first, it passes through several convolutional, batch normalization, activation and max pooling layers while in 		    the other branch it passes through a convolutional layer and a batch normalization only. The outputs of these two branches are then added together to constitute the residual block and then 		    LeakyReLu is performed on the the output of the residual block.
 
         """
         shortcut = y
@@ -134,6 +134,7 @@ def residual_network(x):
         y = layers.LeakyReLU()(y)
 
         return y
+
 
     # Now we can use the defined functions "add_common_layers" and "residual_block" to build the architecture of our ResNet model.
 
@@ -171,86 +172,86 @@ def residual_network(x):
 
     return x
 
+if __name__ == '__main__':
 
-image_tensor = layers.Input(shape=(img_height, img_width, img_channels))   #We instantiate a Keras tensor as input of the network.
-network_output = residual_network(image_tensor)    
-  
-model = models.Model(inputs=[image_tensor], outputs=[network_output])   #Once instantiated the model architecture and the input, we can create the model with this class that groups layers into an object 										 with training and inference features.
-print(model.summary())    #We print a summary of the created model, with the output size and the number of parameters for each layer.
-    
+    image_tensor = layers.Input(shape=(img_height, img_width, img_channels))   #We instantiate a Keras tensor as input of the network.
+    network_output = residual_network(image_tensor)    
+	  
+    model = models.Model(inputs=[image_tensor], outputs=[network_output])   #Once instantiated the model architecture and the input, we can create the model with this class that groups layers into an object 										 with training and inference features.
+    print(model.summary())    #We print a summary of the created model, with the output size and the number of parameters for each layer.
+	    
 
-# definition of lr of the optimizer
-rmsprop = optimizers.RMSprop(lr=0.1)    #Optimizer that implements the RMSprop algorithm, with a learning rate of 0.1.
+	# definition of lr of the optimizer
+    rmsprop = optimizers.RMSprop(lr=0.1)    #Optimizer that implements the RMSprop algorithm, with a learning rate of 0.1.
 
-# definition of callbacks
+	# definition of callbacks
 
-reduce_lr_val = ReduceLROnPlateau(monitor='val_loss', factor=0.1,              
-                   patience=15, min_lr=0.001, verbose = 1)          ##This callback monitors a quantity and if no improvement is seen for a 'patience' number of epochs, the learning rate is reduced by a 										certain factor until a minimum value.
-           
+    reduce_lr_val = ReduceLROnPlateau(monitor='val_loss', factor=0.1,              
+		           patience=15, min_lr=0.001, verbose = 1)          ##This callback monitors a quantity and if no improvement is seen for a 'patience' number of epochs, the learning rate is reduced by a 										certain factor until a minimum value.
+		   
 
-filepath="CC_R_model/weights-improvement-{epoch:02d}-{val_acc:.2f}.h5"         #Path to save the model file
-checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')                          #Callback to save the Keras model or model weights at some frequency.
-callbacks_list = [checkpoint]
+    filepath="CC_R_model/weights-improvement-{epoch:02d}-{val_acc:.2f}.h5"         #Path to save the model file
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')                          #Callback to save the Keras model or model weights at some frequency.
+    callbacks_list = [checkpoint]
 
-def on_epoch_end(self, epoch, logs=None):
-    print(keras.eval(self.model.optimizer.lr))              #This is called at the end of an epoch during training.
-
-
-
-sgd =keras.optimizers.SGD(lr=0.1, decay=1e-1, momentum=0.9, nesterov=True)     #Gradient descent (with momentum) optimizer.
-
-# definition of the inputs via ImageDataGenerator
-
-train_datagen = ImageDataGenerator(              #The built-in class ImageDataGenerator generates batches of tensor image data with real-time data augmentation each time the data are looped over, which 							   means that images are randomly augmented at runtime. This class also allows defining the image normalization (rescale).
-	rescale=1./255,
-        zoom_range=0.2,
-	samplewise_center = True,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        rotation_range=10,
-        horizontal_flip=False)                   #We set the horizontal flip option to false because we train the model on a single projection at a time. So, by flipping the image horizontally we obtain 							  another view. You can try a different data augmentation by changing the other class arguments.
+    def on_epoch_end(self, epoch, logs=None):
+        print(keras.eval(self.model.optimizer.lr))              #This is called at the end of an epoch during training.
 
 
-val_datagen = ImageDataGenerator(
-        samplewise_center = True,
-	rescale=1./255)
-   
+    sgd =keras.optimizers.SGD(lr=0.1, decay=1e-1, momentum=0.9, nesterov=True)     #Gradient descent (with momentum) optimizer.
 
-train_generator = train_datagen.flow_from_directory(                                            #Takes the path to a directory and generates batches of augmented data.
-        '/TrainingSet/CC_R/train',   #Insert your own path to the directory containing the training set of images of the selected projection. This 													model is trained on a single projection at a time. In this example we train the model on the CC_R 													projection. Then train the model separately on the other projections (CC_L, MLO_R, MLO_L). 										
-        batch_size = batch_size,
-        target_size=(img_width, img_height),
-        color_mode = 'rgb',     #If you set the number of channels to 3, you have to set color_mode ='rgb', when you use 1 single channel, you have to set color_mode = 'grayscale'.
-        shuffle = True,
-        class_mode='categorical')
+	# definition of the inputs via ImageDataGenerator
 
-validation_generator = val_datagen.flow_from_directory(
-        '/ValidationSet/CC_R/validation', #Insert your own path to the directory containing the validation set of images of the selected projection (e.g.CC_R)
-        batch_size = batch_size,
-        target_size=(img_width, img_height),
-        color_mode = 'rgb',
-        shuffle = True,
-        class_mode='categorical')
-
-tensorboard = TensorBoard(log_dir="CC_R_model/logs/{}".format(time()), histogram_freq=0, batch_size=4, write_grads=True, write_images=True)   #This is 															optional. It enables visualizations for TensorBoard, a visualization tool provided with 															TensorFlow. It could be useful to visualize the model graph to check that the 																trained model’s structure matches our intended design, if the layers are built 																correctly and the shapes of inputs and outputs of the nodes are those expected. 															Insert tensorboard as callback in model.fit_generator. After training, if you have 																installed TensorFlow with pip, you should be able to launch TensorBoard from the 																command line: tensorboard --logdir=path_to_your_logs
-
-model.compile(optimizer='sgd',                    #Configures the model for training.
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit_generator(train_generator,    #Fits the model on data yielded batch-by-batch by a Python generator.
-          steps_per_epoch=227,           #It is the ratio between the number of images in the training set and the batch size. Approximated to the nearest integer by default.
-          epochs = 100,
-          validation_data=validation_generator,
-          validation_steps=38,              #It is the ratio between the number of images in the validation set and the batch size. Approximated to the nearest integer by default.
-          callbacks=[reduce_lr_val, checkpoint, tensorboard])
+    train_datagen = ImageDataGenerator(              #The built-in class ImageDataGenerator generates batches of tensor image data with real-time data augmentation each time the data are looped over, which 							   means that images are randomly augmented at runtime. This class also allows defining the image normalization (rescale).
+		rescale=1./255,
+		zoom_range=0.2,
+		samplewise_center = True,
+		width_shift_range=0.2,
+		height_shift_range=0.2,
+		rotation_range=10,
+		horizontal_flip=False)                   #We set the horizontal flip option to false because we train the model on a single projection at a time. So, by flipping the image horizontally we obtain 							  another view. You can try a different data augmentation by changing the other class arguments.
 
 
-np.savetxt('CC_R_model/loss_train.txt', history.history['loss'], delimiter=",")
-np.savetxt('CC_R_model/acc_train.txt', history.history['acc'], delimiter=",")
-np.savetxt('CC_R_model/loss_val.txt', history.history['val_loss'], delimiter=",")
-np.savetxt('CC_R_model/acc_val.txt', history.history['val_acc'], delimiter=",")
+    val_datagen = ImageDataGenerator(
+		samplewise_center = True,
+		rescale=1./255)
+	   
+
+    train_generator = train_datagen.flow_from_directory(                                            #Takes the path to a directory and generates batches of augmented data.
+		'/TrainingSet/CC_R/train',   #Insert your own path to the directory containing the training set of images of the selected projection. This 													model is trained on a single projection at a time. In this example we train the model on the CC_R 													projection. Then train the model separately on the other projections (CC_L, MLO_R, MLO_L). 										
+		batch_size = batch_size,
+		target_size=(img_width, img_height),
+		color_mode = 'rgb',     #If you set the number of channels to 3, you have to set color_mode ='rgb', when you use 1 single channel, you have to set color_mode = 'grayscale'.
+		shuffle = True,
+		class_mode='categorical')
+
+    validation_generator = val_datagen.flow_from_directory(
+		'/ValidationSet/CC_R/validation', #Insert your own path to the directory containing the validation set of images of the selected projection (e.g.CC_R)
+		batch_size = batch_size,
+		target_size=(img_width, img_height),
+		color_mode = 'rgb',
+		shuffle = True,
+		class_mode='categorical')
+
+    tensorboard = TensorBoard(log_dir="CC_R_model/logs/{}".format(time()), histogram_freq=0, batch_size=4, write_grads=True, write_images=True)   #This is 															optional. It enables visualizations for TensorBoard, a visualization tool provided with 															TensorFlow. It could be useful to visualize the model graph to check that the 																trained model’s structure matches our intended design, if the layers are built 																correctly and the shapes of inputs and outputs of the nodes are those expected. 															Insert tensorboard as callback in model.fit_generator. After training, if you have 																installed TensorFlow with pip, you should be able to launch TensorBoard from the 																command line: tensorboard --logdir=path_to_your_logs
+
+    model.compile(optimizer='sgd',                    #Configures the model for training.
+		      loss='categorical_crossentropy',
+		      metrics=['accuracy'])
+
+    history = model.fit_generator(train_generator,    #Fits the model on data yielded batch-by-batch by a Python generator.
+		  steps_per_epoch=227,           #It is the ratio between the number of images in the training set and the batch size. Approximated to the nearest integer by default.
+		  epochs = 100,
+		  validation_data=validation_generator,
+		  validation_steps=38,              #It is the ratio between the number of images in the validation set and the batch size. Approximated to the nearest integer by default.
+		  callbacks=[reduce_lr_val, checkpoint, tensorboard])
 
 
-model.save('CC_R_model/final_weights.h5')
+    np.savetxt('CC_R_model/loss_train.txt', history.history['loss'], delimiter=",")
+    np.savetxt('CC_R_model/acc_train.txt', history.history['acc'], delimiter=",")
+    np.savetxt('CC_R_model/loss_val.txt', history.history['val_loss'], delimiter=",")
+    np.savetxt('CC_R_model/acc_val.txt', history.history['val_acc'], delimiter=",")
+
+
+    model.save('CC_R_model/final_weights.h5')
 
